ace47751b852a9fd1c4e846a41db1775cd0ba9a4
Manoj <manojpatil9147@gmail.com>
2024-12-11 08:44:54 +0530

    added fileter to demo users

--- QRcode/admin.py
+++ QRcode/admin.py
@@ -4,7 +4,9 @@ from .models import DemoUser, CurrentFoodCheck
 @admin.register(DemoUser)
 class DemoUserAdmin(admin.ModelAdmin):
     list_display = ('name','id','unique_code', 'role', 'team_name', 'lunch1', 'dinner', 'breakfast', 'lunch2', 'lunch1_completed', 'dinner_completed', 'breakfast_completed', 'lunch2_completed')
-    search_fields = ('name', 'role', 'team_name')
+    search_fields = ('name', 'role', 'team_name','id')
+    list_filter = ('role', 'lunch1', 'dinner', 'breakfast', 'lunch2', 'lunch1_completed', 'dinner_completed', 'breakfast_completed', 'lunch2_completed')
+
 
 @admin.register(CurrentFoodCheck)
 class CurrentFoodCheckAdmin(admin.ModelAdmin):
